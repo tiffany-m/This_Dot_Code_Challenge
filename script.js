0434819d505c427b4c511576e2a5226163a9a883
Tiffany <tiffany_mollica@yahoo.com>
2021-04-10 11:05:02 -0400

    updated styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.github.com/search/users/';
+const API_URL = 'https://api.github.com/search/users?q=';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
@@ -6,11 +6,16 @@ const main = document.getElementById('main');
 
 getUser('tiffany-m');
 
-async function getUser(user) {
-  const res = await fetch(API_URL + user);
-  const data = await res.json();
+async function getUser(userName) {
+  const res = await fetch(API_URL + userName);
+  const body = await res.json();
+  const user = body.items[0];
 
-  createCard(data);
+  const followersRes = await fetch(user.followers_url);
+  const followersBody = await followersRes.json();
+  const followerCount = followersBody.length;
+
+  createCard(user);
 }
 
 function createCard(user) {
